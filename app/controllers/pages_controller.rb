78df44fe99a6f653c1ea874e17b3e852748e23f7
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 17:15:49 +0000

    Integrated skills via model generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,7 @@
 class PagesController < ApplicationController
   def home
-<<<<<<< HEAD
-<<<<<<< HEAD
-    @posts =Blog.all
-     @skills = Skill.all
-=======
->>>>>>> model-generator
-=======
     @posts = Blog.all
     @skills = Skill.all
->>>>>>> model-generator
   end
 
   def about
